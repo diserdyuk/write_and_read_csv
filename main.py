0b8f205fdbfc104ce876fd77665f2eef849fcaba
Denis Serdyuk <diserdyuk@gmail.com>
2020-10-31 16:23:40 +0200

    Add 2 variant read file

--- main.py
+++ main.py
@@ -26,8 +26,26 @@ def main():    # hub func.
 
     cars = [car1, car2, car3]    # pack dict to one container
 
-    for i in cars:
-        write_csv2(i)
+    # for i in cars:
+        # write_csv2(i)
+
+    # read var1
+    with open('finance_yahoo.csv') as f:    # read file    
+        orders = ['symbol', 'price', 'url']
+        read = csv.DictReader(f, fieldnames=orders)
+
+        for i in read:
+            print(i)
+
+    # read var2   
+    '''
+    with open('finance_yahoo.csv') as file:
+        read = csv.reader(file)
+
+        for i in read:
+            print(i)
+    '''
+
 
 
 if __name__ == '__main__':
