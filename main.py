4d4b64ca173478213e177a85e42b3e1f668e80a8
Denis Serdyuk <diserdyuk@gmail.com>
2020-10-31 15:50:41 +0200

    Add 2nd var write data to csv

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import csv
 
 
 
-def write_csv(d):
+def write_csv(d):    # var1 func.for write data to csv
     with open('cars.csv', 'a') as f:
         write = csv.writer(f)
         write.writerow((d['brend'],
@@ -10,16 +10,24 @@ def write_csv(d):
                         d['year']))
 
 
-def main():
-    car1 = {'brend': 'bmw', 'model': '540', 'year': 5}
-    car2 = {'brend': 'audi', 'model': 's7', 'year': 2}
-    car3 = {'brend': 'reno', 'model': 'p5', 'year': 8}
+def write_csv2(d):    # var2 func.for write data to csv
+    with open('cars2.csv', 'a') as f:
+        order_rec = ['brend', 'model', 'year']
+        write = csv.DictWriter(f, fieldnames=order_rec)    # dictwriter, class for write dictionary
+        
+        write.writerow(d)
 
-    cars = [car1, car2, car3]
 
-    # for i in cars:
-        # print(i)
 
+def main():    # hub func.
+    car1 = {'brend': 'bmw', 'model': '540', 'year': 2015}
+    car2 = {'brend': 'audi', 'model': 's7', 'year': 2020}
+    car3 = {'brend': 'reno', 'model': 'p5', 'year': 2018}
+
+    cars = [car1, car2, car3]    # pack dict to one container
+
+    for i in cars:
+        write_csv2(i)
 
 
 if __name__ == '__main__':
